0b87b08048ad34229e09e93ed8870385838d09d7
Mohammed Umair Shaikh <119028682+Umair619@users.noreply.github.com>
2023-08-22 17:12:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const deleteButton = document.querySelector("#delete-btn");
 let userText = null;
 
 // Your OpenAI API key (Replace with your actual API key)
-const API_KEY = "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";
+const API_KEY = "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";
 
 // Function to load chat history and theme from local storage
 const loadDataFromLocalstorage = () => {
